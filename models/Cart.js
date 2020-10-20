b453bb5e47ab5f8029f9f3c26746934c93cd4b6e
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-10-20 11:33:26 +0530

    Structured Cart model

--- models/Cart.js
+++ models/Cart.js
@@ -1,5 +1,25 @@
 const mongoose = require("mongoose");
-const cartSchema = new mongoose.Schema({
-  products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-});
+const cartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    products: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: Number,
+        price: Number,
+        title: "String",
+      },
+    ],
+    active: { type: Boolean, default: true },
+    modifiedOn: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
