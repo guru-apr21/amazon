121b6c1f7e31e8546d9145386df4f40df4843d13
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-10-20 11:32:15 +0530

    Implemented a authentication middleware for verifying jwt token from client requests

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const authenticateJwt = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const decoded = jwt.verify(token, process.env.jwtPrivateKey);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      res.status(403);
+    }
+  } else {
+    res.status(401);
+  }
+};
+
+module.exports = authenticateJwt;
