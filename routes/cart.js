332ff68e74b217bf0774153640a099723ffd7ad6
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-10-20 13:24:53 +0530

    Implemented both get and post for cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const router = express.Router();
 const Cart = require("../models/Cart");
+const User = require("../models/Users");
 
 const authenticateJwt = require("../middleware/auth");
 
-router.post("/cart", authenticateJwt, async (req, res) => {
+router.get("/", authenticateJwt, async (req, res) => {
+  const cart = await Cart.findOne({ userId: req.user._id }).populate("userId");
+  res.json(cart);
+});
+
+router.post("/", authenticateJwt, async (req, res) => {
   try {
     const { productId, quantity, title, price } = req.body;
-
     const { _id: userId } = req.user;
-    let cart = await Cart.findOne({ userId });
+    console.log(userId);
+
+    let cart = await Cart.findOne({ userId }).populate("User", User);
+    console.log(cart);
+
     if (cart) {
       const itemIndex = cart.products.findIndex(
-        (p) => p.productId === productId
+        (p) => String(p.productId) === productId
       );
-      itemIndex > -1
-        ? (cart.products[itemIndex].quantity = quantity)
-        : cart.products.push({ productId, quantity, title, price });
+      console.log("ItemIndex ", itemIndex);
+
+      if (itemIndex > -1) {
+        cart.products[itemIndex].quantity = quantity;
+      } else {
+        cart.products.push({ productId, quantity, title, price });
+      }
+
       cart = await cart.save();
       res.status(201).json(cart);
     } else {
@@ -32,3 +46,5 @@ router.post("/cart", authenticateJwt, async (req, res) => {
     res.send("Something went wrong!");
   }
 });
+
+module.exports = router;
