533fc6e2cdf224c9727a85a23e6a2bc37e07950d
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-10-26 16:51:02 +0530

    Added comments to services

--- services/bcrypt.js
+++ services/bcrypt.js
@@ -1,9 +1,23 @@
 const bcrypt = require("bcrypt");
 
+/**
+ *
+ * @param {String} password
+ * @param {Hash} hash
+ *
+ * @returns {Boolean}
+ *
+ */
 const comparePassword = async (password, hash) => {
   return await bcrypt.compare(password, hash);
 };
 
+/**
+ *
+ * @param {String} password
+ *
+ * @returns hash
+ */
 const hashPassword = async (password) => {
   const hash = await bcrypt.hash(password, 10);
   return hash;
