2ce9f8dec0def3444900b04b4d8710429c68ba35
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-10-26 08:05:44 +0530

    Added comments

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,11 @@
 const Cart = require("../models/Cart");
 const userController = require("./userController");
 
+const findCartByUserId = async (id) => {
+  const cart = await Cart.findOne({ userId: id }).populate("userId");
+  return cart;
+};
+
 /*Query the Cart collection from db by userId and returns 
 if available ,If not returns a message with 400 status*/
 const getCartItems = async (req, res) => {
@@ -11,96 +16,94 @@ const getCartItems = async (req, res) => {
   res.json(cart);
 };
 
+/**
+ * Query the db for cart with given userId
+ * If found add items to the cart
+ * Or else create new cart
+ */
 const createCart = async (req, res) => {
-  const { ...product } = req.body;
   const { _id: userId } = req.user;
+  const { productId, quantity, title, price } = req.body;
 
   const user = await userController.findUserById(userId);
   if (!user) return res.status(400).send("No user with the given id");
 
-  let cart = await findCartByUserId(userId);
+  let cart = await Cart.findOne({ userId }).populate("userId");
 
   if (cart) {
-    cart = await addProdToCart(cart, product);
+    const itemIndex = cart.products.findIndex(
+      (p) => String(p.productId) === productId
+    );
+
+    /*
+     *Find the index of item in the products array
+     *
+     *If < 0 push product into products array
+     *If > 0 update the quantity
+     *
+     */
+
+    if (itemIndex > -1) {
+      cart.products[itemIndex].quantity = quantity;
+    } else {
+      cart.products.push({ productId, quantity, title, price });
+    }
+    cart = await cart.save();
+
     res.status(201).json(cart);
   } else {
-    let newCart = await createNewCart(userId, [product]);
+    /**
+     * Create new cart if the user has no cart
+     */
+    cart = new Cart({
+      userId,
+      products: [{ productId, quantity, title, price }],
+    });
+    cart = await cart.save();
+    cart.populate("userId").execPopulate();
     res.status(201).json(newCart);
   }
 };
 
+/**
+ *
+ * Query the db for cart with given userID
+ * Remove the item from products array
+ *
+ */
 const removeProdFromCart = async (req, res) => {
   const productId = req.body.id;
   const userId = req.user._id;
 
   let cart = await findCartByUserId(userId);
-  cart = await removeProductFromCart(cart, productId);
-  if (cart.products.length < 1) return res.json("Cart is empty");
-  res.status(201).json(cart);
-};
-
-const emptyCart = async (req, res) => {
-  const userId = req.user._id;
-  let cart = await findCartByUserId(userId);
-  if (!cart || cart.products.length < 1)
-    return res.status(400).json("Cart is empty already");
-  await clearCart(cart);
-  res.json("Cart is emptied");
-};
-
-//
-//
-//
-//
-//
-
-const findCartByUserId = async (id) => {
-  const cart = await Cart.findOne({ userId: id }).populate("userId");
-  return cart;
-};
-
-const createNewCart = async (userId, products) => {
-  let cart = new Cart({ userId, products });
-  cart = await cart.save();
-  cart.populate("userId").execPopulate();
-  return cart;
-};
-
-const addProdToCart = async (cart, product) => {
-  const { productId, quantity, title, price } = product;
-
-  const itemIndex = cart.products.findIndex(
-    (p) => String(p.productId) === productId
-  );
-
-  if (itemIndex > -1) {
-    cart.products[itemIndex].quantity = quantity;
-  } else {
-    cart.products.push({ productId, quantity, title, price });
-  }
-  cart = await cart.save();
-  return cart;
-};
-
-const removeProductFromCart = async (cart, productId) => {
   cart.products = cart.products.filter(
     (p) => String(p.productId) !== productId
   );
   cart = await cart.save();
-  return cart;
+
+  if (cart.products.length < 1) return res.json("Cart is empty");
+  res.status(201).json(cart);
 };
 
-const clearCart = async (cart) => {
+/**
+ *
+ * Remove all items from the products array
+ *
+ */
+const emptyCart = async (req, res) => {
+  const userId = req.user._id;
+  let cart = await findCartByUserId(userId);
+
+  if (!cart || cart.products.length < 1)
+    return res.status(400).json("Cart is empty already");
+
   cart.products = [];
   cart = await cart.save();
+  res.json("Cart is emptied");
 };
 
 module.exports = {
   findCartByUserId,
-  createNewCart,
-  addProdToCart,
-  removeProductFromCart,
-  clearCart,
   getCartItems,
   createCart,
   removeProdFromCart,
---
+++ eg.ts
@@ -0,0 +1,3 @@
+function example(foo:string,bar:number){
+
+}
