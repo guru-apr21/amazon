811fdcd56b285204b6984bfe155eac1416eb8795
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-10-27 08:11:01 +0530

    Even with master

--- services/bcrypt.js
+++ services/bcrypt.js
@@ -9,7 +9,8 @@ const bcrypt = require("bcrypt");
  *
  */
 const comparePassword = async (password, hash) => {
-  return await bcrypt.compare(password, hash);
+  const match = await bcrypt.compare(password, hash);
+  return match;
 };
 
 /**
