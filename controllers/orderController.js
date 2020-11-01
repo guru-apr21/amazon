d2f55d870d0f88773f39e3bdee6eca0f6589c157
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-11-01 12:11:13 +0530

    Added payment route

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,9 @@
 const Order = require("../models/Order");
+const Product = require("../models/Product");
+const mongoose = require("mongoose");
+const stripe = require("stripe")(
+  "sk_test_51HhUbGDRaW3L2zxrOz9d8TOvQRpmQxM489GvgsN0IXlKJS4lN5LK4YUn3INZ2wWeQfwtwZAjKuT5sJHXnqJn5NDj00XE57kVEF"
+);
 
 /**
  *
@@ -94,26 +99,59 @@ const deleteOrder = async (req, res, next) => {
  *
  * sets isPaid,paidAt,payment properties and save the order document
  */
-const payForOrder = async (req, res) => {
+
+const pay = async (req, res, next) => {
   try {
-    const id = req.params.id;
-    let order = await Order.findById(id);
-    if (!order) return res.status(404).json("Order Not Found");
+    const { customer, _id: user } = req.user;
+    const { orderItems, cardDetails } = req.body;
+    let ids = req.body.orderItems.map((p) => p.productId);
+    const products = await Product.find()
+      .where("_id")
+      .in(ids)
+      .select("price")
+      .exec();
 
-    order.isPaid = true;
-    order.paidAt = Date.now();
-    order.payment = "paypal";
-    order = await order.save();
-    order = await order
-      .populate("user")
-      .populate({ path: "orderItems", populate: { path: "productId" } })
-      .execPopulate();
+    let amount = 0;
+    for (let i = 0; i < products.length; i++) {
+      if (orderItems[i].productId === String(products[i]._id))
+        amount += products[i].price * orderItems[i].quantity;
+    }
 
-    res.status(200).json({ messsage: "Order Paid", order });
+    const details = await stripe.customers.retrieve(customer);
+    if (!details.default_source && !cardDetails)
+      return res.status(400).json("No payment source");
+    else if (cardDetails) {
+      const token = await stripe.tokens.create({ card: cardDetails });
+      await stripe.customers.createSource(customer, { source: token.id });
+    }
+    const charge = await stripe.charges.create({
+      amount,
+      currency: "inr",
+      customer,
+    });
+
+    const { paid } = charge;
+    if (!paid) return res.status(402).end();
+
+    const order = new Order({
+      user,
+      isPaid: paid,
+      orderItems,
+      totalPrice: amount,
+      paidAt: Date.now(),
+      stripe_charge: charge.id,
+    });
+    res
+      .status(201)
+      .json({ message: "Order places successfully!", data: order });
   } catch (error) {
-    console.log(error);
-    res.status(500).json("Something went wrong");
+    next(error);
   }
 };
 
-module.exports = { getAllOrders, createOrders, deleteOrder, payForOrder };
+module.exports = {
+  getAllOrders,
+  createOrders,
+  deleteOrder,
+  pay,
+};
